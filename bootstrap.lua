ea42dc370f3cd3adcfceec9b5bb517369fb5c4ce
TheSilasDev <82506137+TheSilasDev@users.noreply.github.com>
2021-05-28 15:49:52 +0300

    Create bootstrap.lua

---
+++ bootstrap.lua
@@ -0,0 +1,6 @@
+-- KEY CHECK
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSilasDev/AutoBroly/main/key.lua"))()
+wait(.2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSilasDev/AutoBroly/main/key%20check.lua"))()
+wait(.2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSilasDev/AutoBroly/main/autobroly.lua"))()
