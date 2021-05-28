d0c5f11db7fcf0327b6aa0b1215aa877ed331263
TheSilasDev <82506137+TheSilasDev@users.noreply.github.com>
2021-05-28 15:45:30 +0300

    Update login.lua

--- login.lua
+++ login.lua
@@ -1,1 +1,10 @@
 
+local PlayerName = game:GetService("Players").LocalPlayer.Name
+local WhiteListedPlayers = {"gamer_9470", "Rayveus"}
+if table.find(WhiteListedPlayers, PlayerName) then
+    print("Whitelist 1 Passed")
+else
+    print("not cool")
+    game:GetService("Players").LocalPlayer:Kick("Not Whitelisted")
+end
+wait(.5)
