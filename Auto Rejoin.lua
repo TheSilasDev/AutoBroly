d3253b39c4bd0732edd239f47a8ab3b156725863
TheSilasDev <82506137+TheSilasDev@users.noreply.github.com>
2021-06-07 18:40:54 +0300

    Delete Auto Rejoin.lua

--- Auto Rejoin.lua
+++
@@ -1,9 +0,0 @@
-game:WaitForChild("CoreGui")
-game.CoreGui:WaitForChild("RobloxPromptGui")
-game.CoreGui.RobloxPromptGui:WaitForChild("promptOverlay")
-_G.xd = true 
-while _G.xd do wait()
-if game:GetService("CoreGui").RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt") then 
-game:GetService("TeleportService"):Teleport(536102540, LocalPlayer)
-end
-end
