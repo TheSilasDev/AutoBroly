97fcd38f4174a73585aeeaa8c6cbdaf040ec314d
TheSilasDev <82506137+TheSilasDev@users.noreply.github.com>
2022-08-25 16:28:44 +0300

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -3,4 +3,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSilasDev/AutoBroly
 wait(.2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSilasDev/AutoBroly/main/key%20check.lua"))()
 wait(.2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSilasDev/AutoBroly/main/login.lua"))()
+wait(.2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSilasDev/AutoBroly/main/autobroly.lua"))()
